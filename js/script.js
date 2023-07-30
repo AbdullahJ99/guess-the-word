bced6b1b059e8c2442c6d2d90e0b85d8b0cbc5c4
Abdullah_J <abdullahj1899@gmail.com>
2023-07-29 20:08:31 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//Letter guess list
+const guessedLetters = document.querySelector(".guessed-letters");
+//Guess! button
+const guessButton = document.querySelector(".guess");
+//Letter guessing input
+const letterInput = document.querySelector(".letter");
+//Word in progress paragraph
+const wordInProgress = document.querySelector(".word-in-progress");
+//Remaining guesses paragraph
+const guessesRemaining = document.querySelector(".remaining");
+//Guess message paragraph
+const guessMessage = document.querySelector(".message");
+//Play Again! button
+const playAgain = document.querySelector(".play-again");
+
+//Word to be guessed
+const word = "magnolia";
+
+const blurWord = function(){
+    for(let letter of word){
+        wordInProgress.innerText += "●";
+    }
+};
+blurWord();
+
+guessButton.addEventListener("click", function(e){
+    e.preventDefault();
+    const letter = letterInput.value;
+    console.log(letter);
+    letterInput.value = "";
+});
